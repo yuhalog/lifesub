5e42dd04f840824e7c68c42b5eed9506ab3c66a5
unknown <youha0930@gmail.com>
2025-05-29 15:18:43 +0900

    update:Security config

--- mysub-infra/src/main/java/com/unicorn/lifesub/mysub/infra/config/SecurityConfig.java
+++ mysub-infra/src/main/java/com/unicorn/lifesub/mysub/infra/config/SecurityConfig.java
@@ -1,10 +1,16 @@
 package com.unicorn.lifesub.mysub.infra.config;
 
+import com.unicorn.lifesub.mysub.infra.config.jwt.JwtAuthenticationFilter;
 import com.unicorn.lifesub.mysub.infra.config.jwt.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -48,5 +54,22 @@ public class SecurityConfig {
         return source;
     }
 
+	@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+               .cors(cors -> cors
+                        .configurationSource(corsConfigurationSource())
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs.yaml", "/v3/api-docs/**", "/actuator/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
+        return http.build();
+    }
 }
