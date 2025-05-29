36840ab2e89b4f2d49d62d3dd45005a596613b05
unknown <youha0930@gmail.com>
2025-05-29 15:15:43 +0900

    update: Security config

--- mysub-infra/src/main/java/com/unicorn/lifesub/mysub/infra/config/SecurityConfig.java
+++ mysub-infra/src/main/java/com/unicorn/lifesub/mysub/infra/config/SecurityConfig.java
@@ -1,16 +1,10 @@
 package com.unicorn.lifesub.mysub.infra.config;
 
-import com.unicorn.lifesub.mysub.infra.config.jwt.JwtAuthenticationFilter;
 import com.unicorn.lifesub.mysub.infra.config.jwt.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -40,24 +34,6 @@ public class SecurityConfig {
             swagger페이지와 로그인, 회원등록 페이지는 인증 안해도 접근하도록 설정함
     - sessionManagement: 세션을 로컬에 저장하지 않도록 함
     */
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .cors(cors -> cors
-                        .configurationSource(corsConfigurationSource())
-                )
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs.yaml", "/v3/api-docs/**", "/actuator/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
-    }
 
     @Bean
     protected CorsConfigurationSource corsConfigurationSource() {
@@ -71,4 +47,6 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
+
 }
